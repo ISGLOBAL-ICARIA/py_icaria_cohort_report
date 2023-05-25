bd5940369db35c3b3da2131d7d97ac51e04877b1
andreubp <andreu.bp@gmail.com>
2023-05-25 18:35:29 +0200

    Adjustment on the levels

--- cohorts.py
+++ cohorts.py
@@ -18,9 +18,10 @@ from pydrive.drive import GoogleDrive
 
 gauth = GoogleAuth()
 drive = GoogleDrive(gauth)
+
 #gauth.LocalWebserverAuth()
 
-def cohort_stopping_sistem(redcap_project,nletter,projectkey,date_='2023-03'):
+def cohort_stopping_sistem(redcap_project,nletter,projectkey,date_='2023-05'):
     """
     :param redcap_project_df: Data frame containing all data exported from the REDCap project
     :type redcap_project_df: pandas.DataFrame
@@ -41,6 +42,7 @@ def cohort_stopping_sistem(redcap_project,nletter,projectkey,date_='2023-03'):
         records_dates_ = (records_dates_[~records_dates_['record_id'].isin([240,239])])
     cohorts_from_this_months = pd.merge(records_dates_,letters_, on='record_id')
 
+
     #print(cohorts_from_this_months.groupby('int_random_letter').count()['record_id'])
     if len(cohorts_from_this_months.groupby('int_random_letter').count())==6 and sum(list(cohorts_from_this_months.groupby('int_random_letter').count()['record_id']>=nletter))==6: #False not in list(cohorts_from_this_months.groupby('int_random_letter').count()['record_id']>=nletter):
         STOP = True
@@ -52,8 +54,6 @@ def cohort_stopping_sistem(redcap_project,nletter,projectkey,date_='2023-03'):
                 el = nletter
             sum_+= el
         nletter_comp = nletter + (nletter*6 - sum_)
-        #print(nletter_comp)
-        #print(nletter_comp)
         if sum(list(cohorts_from_this_months.groupby('int_random_letter').count()['record_id']>=nletter_comp))>=4:
             print("It has been recruited the minimum participants per letter + compensation (" + str(nletter) + ") in, at least, 4 letters, and the alert for this HF needs to stop.")
             STOP = True
@@ -61,7 +61,7 @@ def cohort_stopping_sistem(redcap_project,nletter,projectkey,date_='2023-03'):
 
 
 
-def get_record_ids_range_age(redcap_data,min_age,max_age,date_='2023-03-01'):
+def get_record_ids_range_age(redcap_data,min_age,max_age,date_='2023-05-01'):
     xre = redcap_data.reset_index()
     #end_date = datetime.strptime(date_, "%Y-%m-%d").date()
     end_date = datetime.strptime("2023-0"+str(date.today().month)+"-01", "%Y-%m-%d").date()
@@ -89,16 +89,21 @@ def get_record_ids_range_age(redcap_data,min_age,max_age,date_='2023-03-01'):
 def get_record_ids_nc_cohort(redcap_data, max_age, min_age, nletter,projectkey):
 
     ## HAVING RECEIVED AT LEAST 4 DOSES OF SP
+
+    ## 1 CRITERIA: Having received at least 4 doses of SP
     x = redcap_data
     xres = x.reset_index()
-    sp_doses = x.groupby('record_id')['int_sp'].count()
-    record_id_only_4_doses = x.groupby('record_id').count()[sp_doses==4].index
-    record_id_4_doses = x.groupby('record_id').count()[sp_doses>4].index
+
+    sp_doses = xres[xres['int_sp'] == float(1)].groupby('record_id')['int_sp'].count()
+    record_id_only_4_doses = xres[xres['int_sp'] == float(1)].groupby('record_id').count()[sp_doses == 4].index
+    record_id_4_doses = xres[xres['int_sp'] == float(1)].groupby('record_id').count()[sp_doses > 4].index
+
     """ AIXÒ S'HA DE MIRAR BÉ, LO DE L'SP < 14 DIES. PERQUÈ LES XIFRES VAN VARIANT MOLT"""
     ## Calculating if last SP dose was administered  more than 14 days before
     #print(record_id_only_4_doses)
 
     last_SP = xres[(xres['int_sp']==1)&(xres['record_id'].isin(record_id_only_4_doses))].groupby('record_id')['int_date'].last().reset_index()
+
     more_14days = []
     for k,el in last_SP.T.items():
         days_from_SP = datetime.today() - datetime.strptime(el['int_date'],"%Y-%m-%d %H:%M:%S")
@@ -113,11 +118,10 @@ def get_record_ids_nc_cohort(redcap_data, max_age, min_age, nletter,projectkey):
 
     #print(record_id_only_4_doses)
     record_id_4_doses = list(record_id_4_doses)
-    #print(record_id_4_doses)
     for el in list(record_id_only_4_doses):
         if el not in record_id_4_doses:
             record_id_4_doses.append(el)
-    #print(record_id_4_doses)
+#    print(record_id_4_doses)
 
     ## RECORDS THAT MEET THE MAX-MIN AGE RANGE CRITERIA
     records_range_age = get_record_ids_range_age(redcap_data, min_age, max_age)
@@ -146,12 +150,15 @@ def get_record_ids_nc_cohort(redcap_data, max_age, min_age, nletter,projectkey):
                                    (~xres['record_id'].isin(list(completed_participants))) &
                                    (xres['redcap_event_name'] == 'epipenta1_v0_recru_arm_1')][
         ['record_id','study_number', 'int_random_letter']]
-
     all = letters_to_be_contacted[['record_id','study_number','int_random_letter']].reset_index(drop=True)
     all['Recruited'] = letters_to_be_contacted['record_id'].isin(list(already_cohorts)).values
     summary = letters_to_be_contacted.groupby('int_random_letter').count().rename(columns={'record_id':'eligible'})[['eligible']]
     letters_yet_to_be_contacted = letters_to_be_contacted[~letters_to_be_contacted['record_id'].isin(list(already_cohorts))].rename(columns={'record_id':'pending'}).groupby('int_random_letter')['pending']
+    #print(letters_yet_to_be_contacted.groups)
     already_cohorts_letters = xres[(xres['redcap_event_name']=='epipenta1_v0_recru_arm_1')&(xres['record_id'].isin(list(already_cohorts)))&(~xres['int_random_letter'].isnull())][['record_id','study_number','study_number','int_random_letter']].drop_duplicates().rename(columns={'record_id':'recruited'}).groupby('int_random_letter')['recruited']
+    #print(already_cohorts_letters.groups)
+
+
     summary=summary.join(already_cohorts_letters.count()).join(letters_yet_to_be_contacted.count())
     stop=cohort_stopping_sistem(redcap_data,nletter=nletter,projectkey=projectkey)
     #print(stop)
@@ -196,17 +203,18 @@ def excel_creation(project_key,redcap_project, redcap_project_df, excelwriter, s
             dict_to_excel = pd.DataFrame(data=new_dict)
             dict_to_excel.to_excel(excelwriter,project_key,index=False)
         print("COHORT recruitment writen.")
-
         summary_sheet = summary_excel_creation(project_key,redcap_project_df,summarywriter,all_to_FW,summary,stop)
         return summary_sheet
 def summary_excel_creation(project_key,redcap_project_df,summarywriter,all_to_FW,summary,stop):
-    summary_sheet = pd.DataFrame(index=[project_key],columns=['A-elegible','A-recruited','B-elegible','B-recruited',
-        'C-elegible','C-recruited','D-elegible','D-recruited','E-elegible','E-recruited','F-elegible','F-recruited',])
+    summary_sheet = pd.DataFrame(index=[project_key],columns=['HF','A-elegible','A-recruited','B-elegible','B-recruited',
+        'C-elegible','C-recruited','D-elegible','D-recruited','E-elegible','E-recruited','F-elegible','F-recruited','stop'])
     list_to_summary = []
     summary[np.isnan(summary)] = 0
+    list_to_summary.append(project_key)
     for k,el in summary.T.items():
         list_to_summary.append(int(el.eligible))
         list_to_summary.append(int(el.recruited))
+    list_to_summary.append(stop)
     try:
         summary_sheet.loc[project_key] = list_to_summary
     except:
